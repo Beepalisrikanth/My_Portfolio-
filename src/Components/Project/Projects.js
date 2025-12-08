068097542337ac96a8cab7a308e5a574aa4c1bc3
beepali srikanth <beepalisrikanth@gamil.com>
2025-12-08 14:34:30 +0530

    adding new project and fixing some bugs

--- src/Components/Project/Projects.js
+++ src/Components/Project/Projects.js
@@ -3,7 +3,7 @@ import './Projects.css'
 import p1 from '../../assests/felxiPose.webp';
 import p2 from '../../assests/housie.png';
 import p3 from '../../assests/palamuru_university.webp';
-// import p4 from '../../assests/portfolio-4.png';
+import p4 from '../../assests/password_generator.png';
 // import p5 from '../../assests/portfolio-5.png';
 // import p6 from '../../assests/portfolio-6.png';
 
@@ -37,7 +37,6 @@ const Projects = () => {
           </div>
         </div>
 
-
         <div className='projectblock'>
           <img src={p2} alt="project pic argichain" className='projectblockimg'></img>
           <p className='projectblocktitle'>Housie Game</p>
@@ -55,14 +54,11 @@ const Projects = () => {
           </div>
         </div>
 
-
         <div className='projectblock'>
           <img src={p3} alt="university website " className='projectblockimg'></img>
           <p className='projectblocktitle'>University Website Clone</p>
           <p className='projectblocksubtitle'>Responsive Multi-Page Educational Portal</p>
-          <p className='projectblockdesc'> A basic clone of the Palamuru University website built with HTML, CSS, and JavaScript.
-            It replicates the structure and design of a real university site, including navigation menus,
-            announcements, events, and departmental sections for practice in frontend development.</p>
+          <p className='projectblockdesc'> A basic clone of the Palamuru University website built with HTML, CSS, and JavaScript. It replicates the structure and design of a real university site, including navigation menus, announcements, events, and departmental sections for practice in frontend development.</p>
           <div className='projectblockskills'>
             <span className='projectblockskill'> HTML</span>
             <span className='projectblockskill'> CSS</span>
@@ -76,53 +72,23 @@ const Projects = () => {
         </div>
 
         <div className='projectblock'>
-          <img src={p2} alt=" project pic Binance Trading Bot" className='projectblockimg'></img>
-          <p className='projectblocktitle'>Binance Futures Trading Bot </p>
-          <p className='projectblocksubtitle'>Automated Crypto Trading with Python</p>
-          <p className='projectblockdesc'> A Python-based crypto trading bot for Binance USDT-M Futures Testnet.
-            It supports Market and Limit orders, secure API key handling, detailed logging,
-            and a simple Command-Line Interface for real-time trading.</p>
+          <img src={p4} alt="Password Generator Project" className='projectblockimg'></img>
+          <p className='projectblocktitle'>Password Generator </p>
+          <p className='projectblocksubtitle'>Strong & Secure Password Generator Web App</p>
+          <p className='projectblockdesc'> A fast and customizable password generator that allows users to generate secure passwords with options like upper/lowercase letters, numbers, symbols, and adjustable length. It includes copy-to-clipboard features, strength indicators, and modern UI for better user experience.</p>
           <div className='projectblockskills'>
-            <span className='projectblockskill'> Python</span>
-            <span className='projectblockskill'> Binance API</span>
-            <span className='projectblockskill'> CLI</span>
-            <span className='projectblockskill'> dotenv</span>
-            <span className='projectblockskill'> Logging</span>
-          </div>
-          <div className='projectblockprojects'>
-            <span className='projectblockproject'>Documation</span>
-            <span className='projectblockproject'>View Code</span>
-            <span className='projectblockproject'>Live Demo</span>
-          </div>
-        </div>
-
-        {/* <div className='projectblock'>
-          <img src={p1} alt=" " className='projectblockimg'></img>
-          <p className='projectblocktitle'>Felxi Pose </p>
-          <p className='projectblocksubtitle'>Smart Motion Tracking with Real-Time PoseNet ai</p>
-          <p className='projectblockdesc'>Flexi Pose is an AI-based system that detects posture and activities like sitting, standing, walking, and sleeping in real time. It helps users monitor physical behavior and maintain a healthy lifestyle using PoseNet and TensorFlow.js.</p>
-          <div className='projectblockskills'> 
             <span className='projectblockskill'> HTML</span>
             <span className='projectblockskill'> CSS</span>
             <span className='projectblockskill'> JavaScript</span>
-            <span className='projectblockskill'> TensorFlow.js</span>
-            <span className='projectblockskill'> PoseNet</span>
-            <span className='projectblockskill'> p5.js</span>
-            <span className='projectblockskill'> ml5.js</span>
           </div>
           <div className='projectblockprojects'>
-            <span className='projectblockproject'>Documation</span>
-            <span className='projectblockproject'>View Code</span>
-            <span className='projectblockproject'>Live Demo</span>
+            <a href='https://github.com/Beepalisrikanth/Password_Generated_by_srikanth?tab=readme-ov-file' className='projectblockproject' target="_blank" rel="noopener noreferrer">Documentation</a>
+            <a href="https://github.com/Beepalisrikanth/Password_Generated_by_srikanth" className='projectblockproject' target="_blank" rel="noopener noreferrer" >View Code</a>
+            <a href="https://password-generated-by-srikanth-gv30.onrender.com" className='projectblockproject' target="_blank" rel="noopener noreferrer" > Live Demo </a>
           </div>
         </div>
-         */}
-
-
+        
       </div>
-
-
-
       <button className='seemore'>See More</button>
     </section>
   )
